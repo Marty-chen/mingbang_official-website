e6f86e2ac400b80d3bf5c40bb6198930a23d661a
Marty <178464215@qq.com>
2020-03-28 18:39:25 +0800

    第二天

--- src/views/Product_detail.vue
+++ src/views/Product_detail.vue
@@ -1,99 +1,123 @@
 <template>
   <section id="container">
-    <div class>
-      <div class="wrap-container clearfix">
-        <div id="main-content">
-          <div
-            class="wrap-box"
-            style="background: #fff; box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.3);"
-          >
-            <!--Start Box-->
-            <div class="zerogrid">
-              <div class="header">
-                <h1>FERRARI CALIFORNIA</h1>
+    <div class="wrap-container clearfix">
+      <div id="main-content">
+        <div
+          class="wrap-box"
+          style="background: #fff; box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.3);"
+        >
+          <!--Start Box-->
+          <div class="zerogrid">
+            <div class="header">
+              <h1>FERRARI CALIFORNIA</h1>
+            </div>
+            <div class="row">
+              <div class="col-2-3">
+                <div class="wrap-col" style="margin-left: 40px">
+                  <el-carousel
+                    height="700px"
+                    :interval="4000"
+                    arrow="hover"
+                    indicator-position="none"
+                  >
+                    <el-carousel-item v-for="(item,index) in swiper" :key="index">
+                      <img :src="item.img" class="swiper_img" alt />
+                    </el-carousel-item>
+                  </el-carousel>
+                </div>
               </div>
-              <div class="row">
-                <div class="col-2-3">
-                  <div class="wrap-col" style="margin-left: 10px">
-                    <el-carousel
-                      height="700px"
-                      :interval="4000"
-                      arrow="hover"
-                      indicator-position="none"
-                    >
-                      <el-carousel-item v-for="(item,index) in swiper" :key="index">
-                        <img :src="item.img" class="swiper_img" alt />
-                      </el-carousel-item>
-                    </el-carousel>
-                  </div>
+              <div class="col-1-3">
+                <div class="wrap-col" style="text-align: left; margin-left: 150px">
+                  <p class="price">￥78,340</p>
+                  <ul class="specs">
+                    <li style="margin: 50px 0 20px 0">
+                      <strong style="font-size:24px;font-weight: bold;">CATEGORIES</strong>
+                      <span style="font-size:24px;font-weight: bold;margin: 0">PARAMETER</span>
+                    </li>
+                    <li>
+                      <strong>Model Number</strong>
+                      <span>12</span>
+                    </li>
+                    <li>
+                      <strong>Texture</strong>
+                      <span>12</span>
+                    </li>
+                    <li>
+                      <strong>Thickness</strong>
+                      <span>12</span>
+                    </li>
+                    <li>
+                      <strong>Width</strong>
+                      <span>12</span>
+                    </li>
+                    <li>
+                      <strong>Color</strong>
+                      <span>12</span>
+                    </li>
+                    <li>
+                      <strong>Feature</strong>
+                      <span>12</span>
+                    </li>
+                    <li>
+                      <strong>Backing</strong>
+                      <span>12</span>
+                    </li>
+                    <li>
+                      <strong>Composition</strong>
+                      <span>12</span>
+                    </li>
+                    <li>
+                      <strong>Weight</strong>
+                      <span>12</span>
+                    </li>
+                    <li>
+                      <strong>MOQ</strong>
+                      <span>12</span>
+                    </li>
+                    <li>
+                      <strong>Use</strong>
+                      <span>12</span>
+                    </li>
+                    <li>
+                      <strong>Packing</strong>
+                      <span>12</span>
+                    </li>
+                    <li>
+                      <strong>Port</strong>
+                      <span>12</span>
+                    </li>
+                    <li>
+                      <strong>Delivery time</strong>
+                      <span>12</span>
+                    </li>
+                  </ul>
                 </div>
-                <div class="col-1-3" >
-                  <div class="wrap-col" style="text-align: left; margin-left: 150px">
-                    <p class="price">￥78,340</p>
-                    <ul class="specs" >
-                      <li style="margin: 80px 0 30px 0">
-                        <strong style="font-size:24px;font-weight: bold;">CATEGORIES</strong>
-                        <span style="font-size:24px;font-weight: bold;margin: 0">PARAMETER</span>
-                      </li>
-                      <li>
-                        <strong>Model Number</strong>
-                        <span>12</span>
-                      </li>
-                      <li>
-                        <strong>Texture</strong>
-                        <span>12</span>
-                      </li>
-                      <li>
-                        <strong>Thickness</strong>
-                        <span>12</span>
-                      </li>
-                      <li>
-                        <strong>Width</strong>
-                        <span>12</span>
-                      </li>
-                      <li>
-                        <strong>Color</strong>
-                        <span>12</span>
-                      </li>
-                      <li>
-                        <strong>Feature</strong>
-                        <span>12</span>
-                      </li>
-                      <li>
-                        <strong>Backing</strong>
-                        <span>12</span>
-                      </li>
-                      <li>
-                        <strong>Composition</strong>
-                        <span>12</span>
-                      </li>
-                      <li>
-                        <strong>Weight</strong>
-                        <span>12</span>
-                      </li>
-                      <li>
-                        <strong>MOQ</strong>
-                        <span>12</span>
-                      </li>
-                      <li>
-                        <strong>Use</strong>
-                        <span>12</span>
-                      </li>
-                      <li>
-                        <strong>Packing</strong>
-                        <span>12</span>
-                      </li>
-                      <li>
-                        <strong>Port</strong>
-                        <span>12</span>
-                      </li>
-                      <li>
-                        <strong>Delivery time</strong>
-                        <span>12</span>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="wrap-box" style="background: #fff; ">
+      <!--Start Box-->
+      <div class="zerogrid">
+        <div class="row">
+          <div class="col-2-3">
+            <div class="wrap-col" style="margin-left: 40px">
+              <div class="header">
+                <h4>PRODUCT DETAILS</h4>
+              </div>
+              <div class="product_img">
+                <img src="../assets/images/product-slide1.jpg" alt="">
+                <img src="../assets/images/product-slide2.jpg" alt="">
+              </div>
+            </div>
+          </div>
+          <div class="col-1-3">
+            <div class="wrap-col" style="text-align: left; margin-left: 150px">
+              <div class="header" style="margin-top: 150px">
+                <h5 style="color: #999">—— RECOMMENDATION ——</h5>
               </div>
             </div>
           </div>
@@ -112,9 +136,9 @@ export default {
         { img: require("../assets/images/product-slide2.jpg") },
         { img: require("../assets/images/slideshow-image3.jpg") }
       ]
-    }
+    };
   }
-}
+};
 </script>
 
 
@@ -122,16 +146,25 @@ export default {
 #container {
   min-width: 1400px;
 }
-  .swiper_img {
+.swiper_img {
   width: 100%;
   height: 700px;
 }
 .specs li {
-  margin-bottom: 10px;
-  
+  margin-bottom: 7px;
 }
 .specs li span {
   text-align: center;
   margin-left: 50px;
+}
+
+.header {
+  margin: 100px 0 30px 0;
+}
+.product_img {
+  width: 100%;
+}
+.product_img img {
+  width: 100%;
 }
 </style>
